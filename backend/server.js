01c15947a01a4f96857b42e5d263864bd1e76b11
Cotamor <cotamor@gmail.com>
2022-07-28 23:13:10 +0900

    Video-22-Create-Signin-Backend-API

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import data from './data.js'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import connectDB from './config/db.js'
-import productRouter from './routes/productRoutes.js'
 import seedRouter from './routes/seederRoutes.js'
+import productRouter from './routes/productRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -14,6 +13,9 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
@@ -21,6 +23,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/seed', seedRouter)
 // routes
 app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message })
+})
 
 const port = process.env.PORT || 8000
 
