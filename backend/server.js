10f72dd0e983a4920b868e7f568fca1a13afba02
Cotamor <cotamor@gmail.com>
2022-08-04 00:57:48 +0900

    Video-42-Upload-Product-Image

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import seedRouter from './routes/seederRoutes.js'
 import productRouter from './routes/productRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import orderRouter from './routes/orderRoutes.js'
+import uploadRouter from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -31,6 +32,7 @@ app.use('/api/seed', seedRouter)
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
+app.use('/api/upload', uploadRouter)
 
 const __dirname = path.resolve()
 
--- frontend/src/screens/ProductEditScreen.jsx
+++ frontend/src/screens/ProductEditScreen.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { Store } from '../Store'
 import { Helmet } from 'react-helmet-async'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, FormGroup } from 'react-bootstrap'
 import { useReducer } from 'react'
 import axios from 'axios'
 import { getError } from '../utils'
@@ -24,6 +24,12 @@ const reducer = (state, action) => {
       return { ...state, loadingUpdate: false }
     case 'UPDATE_FAIL':
       return { ...state, loadingUpdate: false }
+    case 'UPLOAD_REQUEST':
+      return { ...state, loadingUpload: true, errorUpload: '' }
+    case 'UPLOAD_SUCCESS':
+      return { ...state, loadingUpload: false, errorUpload: '' }
+    case 'UPLOAD_FAIL':
+      return { ...state, loadingUpload: false, errorUpload: action.payload }
     default:
       return state
   }
@@ -37,10 +43,11 @@ const ProductEditScreen = () => {
 
   const { state } = useContext(Store)
   const { userInfo } = state
-  const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
-    loading: true,
-    error: '',
-  })
+  const [{ loading, error, loadingUpdate, loadingUpload }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: '',
+    })
 
   const [name, setName] = useState('')
   const [slug, setSlug] = useState('')
@@ -102,6 +109,29 @@ const ProductEditScreen = () => {
     }
   }
 
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const bodyFormData = new FormData()
+    bodyFormData.append('file', file)
+    try {
+      dispatch({ type: 'UPLOAD_REQUEST' })
+      const { data } = await axios.post('/api/upload', bodyFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      })
+      dispatch({ type: 'UPLOAD_SUCCESS' })
+
+      toast.success('Image uploaded successfully')
+      console.log(data)
+      setImage(data.secure_url)
+    } catch (err) {
+      toast.error(getError(err))
+      dispatch({ type: 'UPLOAD_FAIL', payload: getError(err) })
+    }
+  }
+
   return (
     <div>
       <Helmet>
@@ -153,6 +183,13 @@ const ProductEditScreen = () => {
                 onChange={(e) => setImage(e.target.value)}
                 required
               />
+            </Form.Group>
+
+            {/* Image Upload */}
+            <Form.Group className="mb-3" controlId="imageFile">
+              <Form.Label>Upload File</Form.Label>
+              <Form.Control type="file" onChange={uploadFileHandler} />
+              {loadingUpload && <LoadingBox></LoadingBox>}
             </Form.Group>
 
             {/* Category */}
